06fe0e04bdc12afada1430aacbda9b8edad00446
VishalKNigam <vishalnigam1407@gmail.com>
2023-01-22 11:48:19 +0530

    some last things done

--- login.js
+++ login.js
@@ -26,6 +26,10 @@
           }
           loginarray.push(element);
            localStorage.setItem("loginarray",JSON.stringify(loginarray));
+           alert("User Registered Successfully!!");
+           logInForm.style.display = 'block';
+        signUpForm.style.display = 'none'; 
+           
      })
      let email = logInForm.email.value;
      let password = logInForm.password.value; 
@@ -33,7 +37,8 @@
         el.preventDefault();
         if(registered(loginarray)){
             alert("Login Successfully!!")
-            
+            location.replace("./index.html");
+
         }else {
             alert("Please Register yourself!!")
         }
